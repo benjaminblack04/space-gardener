9e0e9c31372aa2fd69dd3513820f6e04cb63f32a
Thibault GAREL <thibault.garel@edu.ece.fr>
2024-11-07 12:39:31 +0000

    Basic main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,75 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "player.h"
+
+using namespace sf;
+using namespace std;
+
+//CircleShape shape(100.f);
+
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+
+void Reset() {
+    
+}
+
+
+void load() {
+    //shape.setFillColor(Color::Blue);
+    Reset();
+}
+
+
+void Update(RenderWindow& window) {
+    // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+    // check and consume events
+    Event event;
+
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+            return;
+        }
+    }
+    
+
+    // Quit Via ESC Key
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+        window.close();
+    }
+
+    
+}
+
+void Render(RenderWindow& window) {
+    window.draw(shape);
+   /* window.draw(paddles[0]);
+    window.draw(paddles[1]);
+    window.draw(ball);
+    window.draw(message);
+    window.draw(score);*/
+}
+
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  RenderWindow window(VideoMode({gameWidth, gameHeight}), "Tile Game");
+  
+  
+  load();
 
   while (window.isOpen()){
-    sf::Event event;
-    while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
-    }
-    window.clear();
-    window.draw(shape);
-    window.display();
+
+      window.clear();
+      Update(window);
+      Render(window);
+      window.display();
+    
   }
+
   return 0;
 }
