fe03c00fa9388ae554961decdb9367910107355f
benjaminblack <benjamin@benjaminblack.me>
2024-11-20 23:16:24 +0000

    Test TinyXML2 with the level format

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,23 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
+#include <tinyxml2.h>
 
 using namespace sf;
 using namespace std;
+using namespace tinyxml2;
 
 CircleShape shape(100.f);
 
 const int gameWidth = 800;
 const int gameHeight = 600;
 
-
 void Reset() {
-    
-}
 
+}
 
 void load() {
     shape.setFillColor(Color::Blue);
     Reset();
 }
-
 
 void Update(RenderWindow& window) {
     // Reset clock, recalculate deltatime
@@ -34,36 +32,38 @@ void Update(RenderWindow& window) {
             return;
         }
     }
-    
 
     // Quit Via ESC Key
     if (Keyboard::isKeyPressed(Keyboard::Escape)) {
         window.close();
     }
-
-    
 }
 
 void Render(RenderWindow& window) {
     window.draw(shape);
-   
+
 }
 
-
 int main(){
-  RenderWindow window(VideoMode({gameWidth, gameHeight}), "Tile Game");
-  
-  
-  load();
+    RenderWindow window(VideoMode({gameWidth, gameHeight}), "Tile Game");
 
-  while (window.isOpen()){
+    {
+        XMLDocument doc;
 
-      window.clear();
-      Update(window);
-      Render(window);
-      window.display();
-    
-  }
+        doc.LoadFile("res/levels/Test_level.xml");
 
-  return 0;
+        const char* title = doc.FirstChildElement("map")->FirstChildElement("layer")->FirstChildElement("data")->GetText();
+        printf( "Name of play (1): %s\n", title );
+    }
+
+    load();
+
+    while (window.isOpen()){
+        window.clear();
+        Update(window);
+        Render(window);
+        window.display();
+    }
+
+    return 0;
 }
