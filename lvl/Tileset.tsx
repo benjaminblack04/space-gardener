c7ce7e349349bf19fb0fed582b48844ff8b08835
benjaminblack <benjamin@benjaminblack.me>
2024-11-20 23:17:02 +0000

    Change some stuff in the test level

--- lvl/Tileset.tsx
+++ lvl/Tileset.tsx
@@ -1,4 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Tileset" tilewidth="32" tileheight="17" tilecount="88" columns="8">
  <image source="../res/tileset.png" width="256" height="187"/>
+ <tile id="0" probability="0.1"/>
+ <tile id="1" probability="0.8"/>
+ <tile id="2" probability="0.8"/>
+ <tile id="3" probability="0.1"/>
+ <tile id="4" probability="0.05"/>
+ <tile id="6" probability="0.5"/>
 </tileset>
