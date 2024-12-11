f41f8609f7d37d9e578086922bda625cb0594c48
Alex Dumbreck <alex@fountainbridge.holdings>
2024-12-11 02:36:12 +0000

    Use namespace to drop prefix

--- src/graphicsHelpers.cpp
+++ src/graphicsHelpers.cpp
@@ -1,7 +1,9 @@
 #include "graphicsHelpers.h"
 
+using namespace sf;
+
 // Set text properties
-void setMenuText(sf::Text& text_field, const sf::Font& font, unsigned int size, const sf::Color& color, const std::string& text, const float gameWidth, const float gameHeight, const float bottomOffset) {
+void setMenuText(Text& text_field, const Font& font, unsigned int size, const Color& color, const std::string& text, const float gameWidth, const float gameHeight, const float bottomOffset) {
     text_field.setFont(font);
     text_field.setCharacterSize(size);
     text_field.setFillColor(color);
@@ -10,8 +12,8 @@ void setMenuText(sf::Text& text_field, const sf::Font& font, unsigned int size, 
     text_field.setPosition(gameWidth * 0.5f, gameHeight - bottomOffset);
 }
 
-void setMenuButton(sf::RectangleShape &button_field, sf::Text &text_field, const sf::Color &button_color, const sf::Color &text_color, const sf::Font& font, unsigned int size, const std::string& text, const float gameWidth, const float gameHeight, float bottomOffset) {
-    button_field.setSize(sf::Vector2f(menu_button_width, menu_button_height));
+void setMenuButton(RectangleShape &button_field, Text &text_field, const Color &button_color, const Color &text_color, const Font& font, unsigned int size, const std::string& text, const float gameWidth, const float gameHeight, float bottomOffset) {
+    button_field.setSize(Vector2f(menu_button_width, menu_button_height));
     button_field.setOrigin(menu_button_width * .5f, menu_button_height * .5f);
     button_field.setFillColor(button_color);
     button_field.setPosition(gameWidth * 0.5f, gameHeight - bottomOffset);
@@ -19,5 +21,3 @@ void setMenuButton(sf::RectangleShape &button_field, sf::Text &text_field, const
     // Set text properties
     setMenuText(text_field, font, size, text_color, text, gameWidth, gameHeight, bottomOffset);
 }
-
-
